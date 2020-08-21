ba03c5c3a5cf055e5425269099d1aa417a4b134e
Kevin Molina <kevmo1987@gmail.com>
2020-08-20 23:38:28 -0700

    Adding image slider to App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Counter from "./components/Counter";
+import ImageSlider from "./components/ImageSlider";
 
 function App() {
   // passing ES6 style functions as props (you don't need "this" keyword, but in regular function expressions you do)
   const add = (a, b) => a + b;
   return (
     <div className="App">
-      <Header
+      {/* <Header
         title="Hello from App"
         myObj={{
           a: 5,
           b: 6,
         }}
-      />
-      <Body
+      /> */}
+      {/* <Body
         family={["Kevin", "Sophia", "Sage", "Judah"]}
         myFunc={add}
         nums={[10, 12]}
         lambdaFunc={(a, b) => a + b}
-      />
+      /> */}
       <Counter initialCount={33} />
-      <Counter initialCount={0} />
+      <ImageSlider />
     </div>
   );
 }
